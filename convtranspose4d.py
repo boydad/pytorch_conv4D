fecdc827069ef69c6a16313ed4ba6ce555eae3e6
Yin Lin <ylin910095@gmail.com>
2023-07-20 17:34:11 -0400

    Toy example: implement conv2dtranspose with conv1dtranspose

---
+++ convtranspose4d.py
@@ -0,0 +1,54 @@
+import numpy as np
+import math, sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def conv2d_tranpose_test():
+    batch_size = 1
+    in_channels = 2
+    out_channels = 3
+    input_size = [2, 2]
+    kernel_size = 2
+    stride = 1
+    input_2d = torch.randn(batch_size, in_channels, *input_size).to(torch.float64)
+    weight_2d = torch.randn(in_channels, out_channels, kernel_size, kernel_size).to(torch.float64)
+    
+    # Debug
+    input_2d = torch.ones_like(input_2d)
+    weight_2d = torch.ones_like(weight_2d)
+    weight_2d[...,1,:] = 0
+
+    true_result = F.conv_transpose2d(input_2d, weight_2d, 
+                bias=None, stride=stride, padding=0, 
+                output_padding=0, groups=1, dilation=1)
+
+    output_size =  [(i-1)*stride + (kernel_size - 1) + 1 for i in input_size]
+    output = torch.zeros([batch_size, out_channels, *output_size], dtype=torch.float64)
+
+    op = lambda x, y, groups: F.conv_transpose1d(
+                x, y, bias=None, stride=stride, padding=0, 
+                output_padding=0, groups=groups, dilation=1) 
+    
+    for iL in range(input_size[0]): # extra dimension!
+        x = input_2d[...,iL,:]
+
+        input_kernel = weight_2d.permute(2, 0, 1, 3)
+        input_kernel = input_kernel.reshape(kernel_size*in_channels, 
+                                            out_channels, 
+                                            *weight_2d.shape[3:])
+
+        # Fun with shapes
+        input_x = x.repeat(1, kernel_size, 1)
+        op_output = op(input_x, input_kernel, groups=kernel_size)
+
+        op_output = op_output.reshape(batch_size, kernel_size, out_channels,
+                                      *op_output.shape[2:])
+        op_output = op_output.transpose(1, 2)
+        output[:, :, iL:iL+kernel_size] += op_output    
+    
+    assert torch.allclose(true_result, output)
+    print("tests passed")    
+
+if __name__ == "__main__":
+    conv2d_tranpose_test()
